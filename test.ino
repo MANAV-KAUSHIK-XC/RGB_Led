45251b3d236e23f74cf81cc771e1f02f90391972
MANAV-KAUSHIK-XC <75835693+MANAV-KAUSHIK-XC@users.noreply.github.com>
2020-12-13 20:03:12 +0400

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,14 @@
+#include <RGB.h>
+
+RGB rgb = RGB(9, 10, 11, false);
+
+void setup() {
+  // put your setup code here, to run once:
+  rgb.lightRGB(RGB::RED);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
